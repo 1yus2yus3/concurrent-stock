72381403f77275cddefc46c6bd4e67504c520b38
cola <870252122@qq.com>
2019-11-22 18:38:37 +0800

    feat:库存在互联网应用从基础到高效

---
+++ src/main/java/com/cola/concurrentstock/ConcurrentStockApplication.java
@@ -0,0 +1,13 @@
+package com.cola.concurrentstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cola/concurrentstock/ConcurrentStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cola.concurrentstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
